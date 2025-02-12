7da5b577bc6dc88975fef6a4cac9a200c7db7c7a
ajay-portpro <ajay@portpro.io>
2025-02-12 19:03:24 +0545

    fix:update vite config file for deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
-  // Load environment variables
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
@@ -12,6 +11,9 @@ export default ({ mode }) => {
     },
     css: {
       postcss: "./postcss.config.js"
-    }
+    },
+    build: {
+      outDir: 'dist', 
+    },
   });
 };
